1bb313550e011684f29f6fbb3bbacd198a1bc187
expermentus <christofferbakaa@gmail.com>
2022-12-05 15:53:17 +0100

    working yolo without fill files

--- main.py
+++ main.py
@@ -4,11 +4,7 @@ import numpy as np
 import cv2
 from time import time
 
-from functorch._src.aot_autograd import model_name
-
-
 class LegoDetection:
-
 
     def __init__(self, capture_index, model_name):
         self.capture_index = capture_index
@@ -49,7 +45,7 @@ class LegoDetection:
         x_shape, y_shape = frame.shape[1], frame.shape[0]
         for i in range(n):
             row=cord[i]
-            if row[4]>= 0.2:
+            if row[4]>= 0.7:
                 x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape)
                 bgr = (0, 255, 0)
                 cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
@@ -89,56 +85,3 @@ detector = LegoDetection(capture_index=0, model_name='./runs/train/yolov5s_resul
 detector()
 
 
-
-
-
-
-
-
-""""""
-#Example of how to connect pymavlink to an autopilot via an UDP connection
-
-
-# Disable "Bare exception" warning
-# pylint: disable=W0702
-
-# Import mavutil
-
-from roboflow import Roboflow
-rf = Roboflow(api_key="n5mOz2UBdtv07UIFuhIF")
-project = rf.workspace("newoysters").project("oysters-4vutw")
-dataset = project.version(41).download("yolov5")
-
-import glob
-from IPython.display import Image, display
-import yaml
-with open(dataset.location + "/data.yaml", 'r') as stream:
-    num_classes = str(yaml.safe_load(stream)['nc'])
-
-from IPython.core.magic import register_line_cell_magic
-#@register_line_cell_magic
-def writetemplate(line, cell):
-    with open(line, 'w') as f:
-        f.write(cell.format(**globals()))
-
-from utils.plots import plot_results
-
-Image(filename='./runs/train/exp2/results.png', width=1000)  # view results.png
-
-
-
-
-
-for imageName in glob.glob('./runs/detect/exp2/*.jpg'): #assuming JPG
-    display(Image(filename=imageName))
-# Create the connection
-#  If using a companion computer
-#  the default connection is available
-#  at ip 192.168.2.1 and the port 14550
-# Note: The connection is done with 'udpin' and not 'udpout'.
-#  You can check in http:192.168.2.2:2770/mavproxy that the communication made for 14550
-#  uses a 'udpbcast' (client) and not 'udpin' (server).
-#  If you want to use QGroundControl in parallel with your python script,
-#  it's possible to add a new output port in http:192.168.2.2:2770/mavproxy as a new line.
-#  E.g: --out udpbcast:192.168.2.255:yourport
-""""""
